681738d8bfcde33df7388f15e4ca927fd9a56b4e
Denis <potekhin@umgoroda.ru>
2019-12-06 12:03:48 +0100

    Simple REST v1.0

--- config/web.php
+++ config/web.php
@@ -15,6 +15,9 @@ $config = [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => 'Y1l-EKkSDCdpJJW0Pyi4ZXfJyy6k0Xjm',
+            'parsers' => [
+                'application/json' => 'yii\web\JsonParser',
+            ],
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
@@ -43,14 +46,16 @@ $config = [
             ],
         ],
         'db' => $db,
-        /*
+
         'urlManager' => [
             'enablePrettyUrl' => true,
+            'enableStrictParsing' => true,
             'showScriptName' => false,
             'rules' => [
+                ['class' => 'yii\rest\UrlRule', 'controller' => 'user'],
             ],
         ],
-        */
+
     ],
     'params' => $params,
 ];
